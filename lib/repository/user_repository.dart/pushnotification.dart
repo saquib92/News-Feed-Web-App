ced757af991f99a1334a05ddf2b46de4081ba011
saquib92 <saquibsheikh094@gmail.com>
2024-09-23 00:01:47 +0530

    Initial commit

--- lib/repository/user_repository.dart/pushnotification.dart
+++ lib/repository/user_repository.dart/pushnotification.dart
@@ -60,9 +60,8 @@ class PushNotifications {
         DarwinInitializationSettings(
       onDidReceiveLocalNotification: (id, title, body, payload) {},
     );
-    final LinuxInitializationSettings initializationSettingsLinux =
-        const LinuxInitializationSettings(
-            defaultActionName: 'Open notification');
+    const LinuxInitializationSettings initializationSettingsLinux =
+        LinuxInitializationSettings(defaultActionName: 'Open notification');
     final InitializationSettings initializationSettings =
         InitializationSettings(
             android: initializationSettingsAndroid,
